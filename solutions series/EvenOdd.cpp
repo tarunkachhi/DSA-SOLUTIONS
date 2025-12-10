efb34686681b945636a8c774dad6d2a9589fd782
tarunkachhi <tarunkachhi@gmail.com>
2025-12-10 15:27:36 +0530

    newcommit

---
+++ solutions series/EvenOdd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+
+    if (num%2==0)
+    {
+        cout << "The given Number is Even" << endl;
+    } else {
+        cout << "The given Number is Odd";
+    }
+    return 0;
+}
